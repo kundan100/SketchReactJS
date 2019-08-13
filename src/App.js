aea64593ee1e5215a74a257b5c07c5543429f7e1
kundan100 <kundan100@yahoo.com>
2019-08-13 17:05:18 +0530

    updates minor message and it's text color for test purpose only

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const redColor = {
+  color: 'red'
+};
+
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={redColor}>Edit done!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
